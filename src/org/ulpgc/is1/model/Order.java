e885228ab9eff1b80844b30f867dcc653aa203d2
Maria <maria.alonso159@alu.ulpgc.es>
2023-10-10 11:38:34 +0100

    addded addDish()

--- src/org/ulpgc/is1/model/Order.java
+++ src/org/ulpgc/is1/model/Order.java
@@ -4,12 +4,12 @@ package org.ulpgc.is1.model;
 public class Order {
 
 
-    private int NEXT_ID = 0;
+    private static int nextID = 0;
     private final int id;
 
 
     public Order(int id) {
-        this.id = id;
+        this.id = nextID++;
     }
 
 
